c081fa1c761380498b19f583c272bba5f4fb46f6
sai16284 <140633424+sai16284@users.noreply.github.com>
2023-10-15 12:04:48 +0530

    Add files via upload
    ok

---
+++ Program1.java
@@ -0,0 +1,21 @@
+package Cloud.Vandana;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Program1 {
+
+	public static void main(String[] args) throws Exception {
+
+		Integer[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+
+		List<Integer> intList = Arrays.asList(intArray);
+
+		Collections.shuffle(intList);
+
+		intList.toArray(intArray);
+
+		System.out.println(Arrays.toString(intArray));
+	}
+}
